1f299bb1467ed46c8f9d7c3cbe153313e10bd5c2
Ines Bakhtaoui <inesbkht@gmail.com>
2021-06-10 09:39:43 +0200

    exercice 3.3 fait

---
+++ exercice3.3.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 3.3</title>
+</head>
+<body>
+    <h1>Exercice 3 Partie 3 : Les boucles PHP</h1>
+    <ul>
+        <?php
+        $firstNumber = 100;
+        $secondNumber = 48;
+        while ($firstNumber >=10){
+        ?>  
+            <li>
+            <?= ($firstNumber.'x' .$secondNumber. ' = ' .$firstNumber*$secondNumber);
+            $firstNumber--;}?>
+            </li>
+    </ul>
